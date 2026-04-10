375e402c27833b347f9cbc5a9278013d0e1d242e
KarthikeyanY28006 <karthik777kn@gmail.com>
2026-04-10 19:21:32 +0530

    Literals in java

---
+++ Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+    public static void main(String[] args) {
+
+        // Type Conversion (Automatic)
+        int a = 100;
+        double b = a;
+
+        // Type Casting (Manual)
+        double x = 45.67;
+        int y = (int) x;
+
+        System.out.println("Conversion: " + b);
+        System.out.println("Casting: " + y);
+    }
+}
