bb8bde208f8f181b9e68ca005d6aaf9c1a791ea0
KarthikeyanY28006 <karthik777kn@gmail.com>
2026-04-10 11:52:08 +0530

    removed file

--- Myname.java
+++
@@ -1,9 +0,0 @@
-class Myname {
-    public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
-    }
-}
