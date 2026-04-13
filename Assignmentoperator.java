44919f66a4a2f76d1b76925e4d72cc41bc4d93a6
KarthikeyanY28006 <karthik777kn@gmail.com>
2026-04-13 13:37:10 +0530

    Assignment Operators in java

---
+++ Assignmentoperator.java
@@ -0,0 +1,19 @@
+public class Assignmentoperator {
+    public static void main (String[] args) {
+        int a = 16;
+        a +=4;
+        System.out.println("Addition Assignment: " + a);
+
+        a -= 4;
+        System.out.println("Subtraction Assignment: " + a);
+
+        a *= 4;
+        System.out.println("Multiplication Assignment: " + a);
+
+        a /= 4;
+        System.out.println("Division Assignment: " + a);
+
+        a %= 4;
+        System.out.println("Modulus Assignment: " + a);
+    }   
+}
---
+++ Operator.java
@@ -0,0 +1,24 @@
+public class Operator {
+    public static void main (String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b; // addition operator
+        System.out.println("Addition: " + sum);
+
+        int c = 15;
+        int d = 25;
+        int difference = d - c; // subtraction operator
+        System.out.println("Difference: " + difference);
+
+        int e = 10;
+        int f = 6;
+        int Multiplication = e * f; // multiplication operator
+        System.out.println("Multiplication: " + Multiplication);
+
+        int g = 20;
+        int h = 4;
+        int Division = g / h; // division operator
+        System.out.println("Division: " + Division);    
+
+    }
+}
