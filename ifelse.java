fa8606e13e035cc62de003963ea8bbf49d587417
KarthikeyanY28006 <karthik777kn@gmail.com>
2026-04-13 15:45:42 +0530

    If else in Java

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+public class ifelse {
+    public static void main (String [] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+
+        if ( a < b)
+        {
+            System.out.println (" a is less than b.");
+        }
+        else
+            System.out.println (" a is not less than b.");
+    }
+}
