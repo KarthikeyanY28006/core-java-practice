8a8794b58cc7804d2f20a934fee386e977c6d2ac
KarthikeyanY28006 <karthik777kn@gmail.com>
2026-04-13 14:41:47 +0530

    If statement in java

---
+++ ifexample.java
@@ -0,0 +1,16 @@
+public class ifexample {
+    public static void main (String [] args) {
+        int a = 5;
+        if ( a < 0)
+        {
+            System.out.println("a is a positive number.");  
+            }
+        else 
+        {
+            System.out.println("a is a negative number.");
+        }
+ }
+}
+// The if statement is a control flow statement that allows you to execute a block of code only if a specified condition is true. The condition is evaluated, and if it is true, 
+// the code block inside the if statement is executed. 
+// If the condition is false, the code block is skipped.          
