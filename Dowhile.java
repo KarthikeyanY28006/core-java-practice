081483ea65a674f53d18171bbbd08ce02abaf8d5
KarthikeyanY28006 <karthik777kn@gmail.com>
2026-04-14 10:35:05 +0530

    Do While Loop in java

---
+++ Dowhile.java
@@ -0,0 +1,10 @@
+public class Dowhile {
+    public static void mnain (String [] args) {
+        int i =1;
+        do
+        {
+            System.out.println("My name is Karthik" + i);
+            i++;
+        }while(i <=50);
+    }
+}
