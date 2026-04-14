cd5d13bceef44a9e257696085cc0ba1d188b8e42
KarthikeyanY28006 <karthik777kn@gmail.com>
2026-04-14 10:31:39 +0530

    While loop in java

---
+++ whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+    public static void main (String [] args ) {
+        int i = 1;
+        while (i <= 50)
+        {
+            System.out.println("My name is Karthik" + i);
+            i++;    
+        }
+    }
+}
